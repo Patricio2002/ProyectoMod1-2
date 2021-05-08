e8d905ca6aad6a5e88b3b7e777621681cd3c9c05
pato <patricio.ad26301@gmail.com>
2021-05-08 18:02:41 -0500

    cambios principal

---
+++ Dynamics/Principal.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+    $_SESSION['']= "seción";
+    echo '
+    <table bgcolor=lightblue>
+    <thead></thead>
+        <tbody>
+            <tr>
+                <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>
+                <td></td>
+                <td><form action="./InicioSesion.php" method=post></td>
+                    <td><input type=submit name=inicio value="INICIAR SESIÓN"></td>
+                </form>
+                <td><form action="./CrearCuenta.php" method=post></td>
+                    <td><input type=submit name=registro value=REGISTRO></td>
+                </form>
+               <td><input type=submit name=coleccion value=SECCIONES></td>
+            </tr>
+        </tbody>
+    </table>
+    <br>';
+    $imagenes=["1"=>"<img src= '../Templates/L1.jpg' width= 400>",
+               "2"=>"<img src= '../Templates/L2.jpg' width= 400>",
+               "3"=>"<img src= '../Templates/L3.jpg' width=400>",
+               "4"=>"<img src= '../Templates/L4.jpg' width= 400>",
+               "5"=>"<img src= '../Templates/L5.jpg' width= 400>"];
+    $r= rand(1, 5);
+    foreach($imagenes as $key=>$value)
+    {
+        if($r==$key)
+        {
+            echo $value;
+        }
+    }
+?>
