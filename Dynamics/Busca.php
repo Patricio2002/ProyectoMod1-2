22c70fe03ce1d7d59e82b269a1e094c9bdb2e1f6
Abe <irandy.cat.9@gmail.com>
2021-05-08 22:09:02 -0500

    Dynamics/Busca.php

--- Dynamics/Busca.php
+++ Dynamics/Busca.php
@@ -36,12 +36,34 @@
     <table bgcolor=lightblue>
     <thead><th>CATEGORÍAS</th></thead>
         <tbody>
-            <tr><td><form action='' method=post></td></tr>
-                <tr><td><input type=submit name=biología value='Ciencias Biológicas y de la salud'></td></tr>
+            <tr><td><form action='Busca.php' method=post></td></tr>
+                <tr><td><input type=submit name=biol value='Ciencias Biológicas y de la salud'></td></tr>
                 <tr><td><input type=submit name=mate value='Ciencias Físico Matemáticas'></td></tr>
                 <tr><td><input type=submit name=sociales value='Ciencias sociales'></td></tr>
                 <tr><td><input type=submit name=lite value='Literatura'></td></tr>
             </form> 
         </tbody>
     </table>";
+    $bio=(isset($_POST["bio"]) && $_POST["bio"]!= "") ?$_POST["bio"]: "No";
+    $mate=(isset($_POST["mate"]) && $_POST["mate"]!= "") ?$_POST["mate"]: "No";
+    $sociales=(isset($_POST["sociales"]) && $_POST["sociales"]!= "") ?$_POST["sociales"]: "No";
+    if($_SESSION['secion']&&$bio!= "No")
+    {
+
+    }
+    elseif($_SESSION['secion']&&$mate!= "No")
+    {
+        
+    }
+    elseif($_SESSION['secion']&&$sociales!= "No")
+    {
+        
+    }
+    else
+    {
+        if(($bio!="No"||$mate!="No"||$sociales!="No")&& !$_SESSION['secion'])
+        {
+            header("location: ./InicioSesion.php");
+        }
+    }
 ?>
