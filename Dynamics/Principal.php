a1d9c88f57831403714c80c95821b9835a7a362b
Abe <irandy.cat.9@gmail.com>
2021-05-12 01:34:20 -0500

    Dynamics/Principal.php

--- Dynamics/Principal.php
+++ Dynamics/Principal.php
@@ -1,25 +1,12 @@
 <?php
     session_start();
-<<<<<<< HEAD
     isset($_SESSION["inicio"]);
     //precentación de la página
     echo '
-=======
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Coyo bidi</title>
-    </head>
-    <body>
->>>>>>> d043245ffa96fbdc6fadd503dd410ecb6c518348
     <table bgcolor=lightblue>
     <thead></thead>
         <tbody>
             <tr>
-<<<<<<< HEAD
                 <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>
                 <td></td>
                 <td><form action="./InicioSesion.php" method=post></td>
@@ -32,22 +19,6 @@
                <td><input type=submit name=coleccion value=Buscar></td>
                </from>
             </tr>
-=======
-                <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>';
-                echo "<td></td>";
-                if(isset($_SESSION["inicio"])){ 
-                    $_SESSION["usuario"]=$_POST["nombre"].$_POST["apellidos"];
-                    echo "<td>$_SESSION[usuario]</td>";
-                    echo "<td><form action=./Cuenta.php method=post></td>";
-                        echo "<td><input type=submit name=Cuenta value=Cuenta></td>";
-                    echo "</form>";
-                    
-                }
-                else{
-                    header("location: InicioSesion.php");
-                }
-            echo '</tr>
->>>>>>> d043245ffa96fbdc6fadd503dd410ecb6c518348
         </tbody>
     </table>
     <br>';
