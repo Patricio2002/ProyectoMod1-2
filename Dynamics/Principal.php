89b97d68b4360c8e99d74348bc1ce152acf4da7b
Abe <irandy.cat.9@gmail.com>
2021-05-11 22:41:47 -0500

    Dynamics/Principal.php

--- Dynamics/Principal.php
+++ Dynamics/Principal.php
@@ -1,28 +1,12 @@
 <?php
     session_start();
-<<<<<<< HEAD
-    $_SESSION['secion']= "seción";
+    isset($_SESSION["inicio"]);
+    //precentación de la página
     echo '
-=======
-    if(isset($_POST["Crear"]))
-    {
-        $_SESSION["inicio"]=1;
-    }
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Coyo bidi</title>
-    </head>
-    <body>
->>>>>>> 41c4efd12c1baf12600aac1fd573ff2df2967896
     <table bgcolor=lightblue>
     <thead></thead>
         <tbody>
             <tr>
-<<<<<<< HEAD
                 <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>
                 <td></td>
                 <td><form action="./InicioSesion.php" method=post></td>
@@ -33,32 +17,12 @@
                 </form>
                 <td><form action="./Busca.php" method=post></td>
                <td><input type=submit name=coleccion value=Buscar></td>
+               </from>
             </tr>
-=======
-                <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>';
-                echo "<td></td>";
-                if(isset($_SESSION["inicio"])){ 
-                    $_SESSION["usuario"]=$_POST["nombre"].$_POST["apellidos"];
-                    echo "<td>$_SESSION[usuario]</td>";
-                    echo "<td><form action=./Cuenta.php method=post></td>";
-                        echo "<td><input type=submit name=Cuenta value=Cuenta></td>";
-                    echo "</form>";
-                    
-                }
-                else{
-                    echo '<td><form action="./InicioSesion.php" method=post></td>
-                        <td><input type=submit name=inicio value="INICIAR SESIÓN"></td>
-                    </form>
-                    <td><form action="./CrearCuenta.php" method=post></td>
-                        <td><input type=submit name=registro value=REGISTRO></td>
-                    </form>
-                    <td><input type=submit name=coleccion value=SECCIONES></td>';
-                }
-            echo '</tr>
->>>>>>> 41c4efd12c1baf12600aac1fd573ff2df2967896
         </tbody>
     </table>
     <br>';
+    //arreglo para desplegar aleatoriamente un libro
     $imagenes=["1"=>"<img src= '../Statics/L1.jpg' width= 400>",
                "2"=>"<img src= '../Statics/L2.jpg' width= 400>",
                "3"=>"<img src= '../Statics/L3.jpg' width=400>",
@@ -72,6 +36,4 @@
             echo $value;
         }
     }
-    ?>
-    </body>
-    </html>
+?> 
