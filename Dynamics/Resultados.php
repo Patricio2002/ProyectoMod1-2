347a7fc105d87b92515fdbbb552da0cbb6d69ca0
Abe <irandy.cat.9@gmail.com>
2021-05-12 23:26:13 -0500

    Dynamics/Resultados.php

--- Dynamics/Resultados.php
+++ Dynamics/Resultados.php
@@ -1,40 +1,7 @@
 <?php
-<<<<<<< HEAD
-    //Inicio
-    echo '
-    <table bgcolor=lightblue>
-    <thead></thead>
-        <tbody>
-            <tr>
-                <td><img src= "../Statics/Logo.png" width= 100 height= 100></td>
-                <td></td>
-                <h2>
-                    <td>V</td>
-                    <td>I</td>
-                    <td>S</td>
-                    <td>T</td>
-                    <td>A</td>
-                    <td></td>
-                    <td>D</td>
-                    <td>E</td>
-                    <td>T</td>
-                    <td>A</td>
-                    <td>L</td>
-                    <td>L</td>
-                    <td>A</td>
-                    <td>D</td>
-                    <td>A</td>
-                </h2>
-                <td><form action="./Busca.php" method=post></td>
-                    <td><input type=submit name=registro value=VOLVER></td>
-                </form>
-            </tr>
-        </tbody>
-    </table>
-    <br>';//Opción de regresar
-=======
     session_start();
     include("./encabezado.php");
+    $conexion=connectdb();
     $array2=array();
     $a=0;
     $i=0;
@@ -43,7 +10,7 @@
     $e=0;
     if($_POST['titulo']!=NULL){
         $a=1;
-        $A="SELECT * FROM libro WHERE titulo LIKE('$_POST[titulo]')";
+        $A=("SELECT * FROM libro WHERE titulo LIKE('$_POST[titulo]')");
         array_push($array2, $A);
     }
     if($_POST['autor']!=NULL){
@@ -81,5 +48,4 @@
     $res=mysqli_query($conexion, $buscar);
    $libro=mysqli_fetch_array($res);
     echo $buscar;
->>>>>>> e9fb99d0e65c01c4e82d09a4927d1b20d1718064
 ?>
